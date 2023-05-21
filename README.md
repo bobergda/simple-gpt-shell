cd9713827795225efe2a611682d660f1c90f79e8
bobergda <bobergda@gmail.com>
2023-05-21 02:28:53 +0200

    Update README.md and add new files gpt-shell.py and requirements.txt to branch main

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # simple-gpt-shell
+
+The `gpt-shell.py` script is a command-line interface for interacting with the OpenAI GPT-3 API. It allows the user to input bash commands for Linux and receive a response from the GPT-3 model with a suggested command to execute. The script uses the `revChatGPT.V3` module to interface with the GPT-3 API and requires an API key to be set as an environment variable. The script is designed to be run in a terminal and provides a system prompt for the user to input commands.
+
+## Installation
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running `pip install -r requirements.txt`.
+3. Set your OpenAI API key as an environment variable named `OPENAI_API_KEY`.
+
+## Usage
+
+1. Open a terminal and navigate to the directory where `gpt-shell.py` is located.
+2. Run the script by typing `python gpt-shell.py` and pressing enter.
+3. Follow the system prompt to input bash commands for Linux.
+4. The script will provide a suggested command to execute based on the input.
+
+Note: The script is designed to be run in a terminal and may not work as expected in other environments.
---
+++ gpt-shell.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+from revChatGPT.V3 import Chatbot
+
+system_prompt = """Provide only bash commands for Linux without any description.
+If there is a lack of details, provide most logical solution.
+Ensure the output is a valid shell command.
+If multiple steps required try to combine them together.
+Before command add "CMD: ".
+"""
+
+api_key = os.getenv("OPENAI_API_KEY", "")
+if api_key == "":
+    print("Error: OPENAI_API_KEY is not set")
+    exit(1)
+
+chatbot = Chatbot(api_key=api_key, system_prompt=system_prompt)
+
+while True:
+    try:
+        prompt = input("ChatGPT: ")
+        print(f"=== Prompt\n{prompt}")
+
+        response = chatbot.ask(prompt)
+        print(f"=== Response\n{response}")
+
+        if response.startswith("CMD: "):
+            command = response[5:]
+
+            print(f"=== Run command\n{command}")
+            process = subprocess.Popen(
+                command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, errors = process.communicate()
+            exit_code = process.wait()
+
+            prompt = f"Analize command output:\n{output.decode()}"
+            print(f"=== Prompt\n{prompt}")
+
+            response = chatbot.ask(prompt)
+            print(f"=== Response\n{response}")
+        else:
+            print("Error: Command not found")
+    except subprocess.CalledProcessError as e:
+        print(
+            f"Error: Command failed with exit code {e.returncode}: {e.output}")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        exit(0)
+    except EOFError:
+        print("Exiting...")
+        exit(0)
+    except Exception as e:
+        print(f"Error of type {type(e).__name__}: {e}")
