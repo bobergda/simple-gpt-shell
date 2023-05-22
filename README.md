a4aab93ad284e993c78c15cb800022f2b8981c41
bobergda <bobergda@gmail.com>
2023-05-23 01:56:53 +0200

    refactor(gpt-shell.py): extract get_api_key function to improve readability and reusability refactor(gpt-shell.py): extract truncate_prompt function to improve readability and reusability refactor(gpt-shell.py): extract run_command function to improve readability and reusability refactor(gpt-shell.py): extract analyze_command function to improve readability and reusability refactor(gpt-shell.py): extract main function to improve readability and reusability feat(gpt-shell.py): add support for truncating input prompt to 1024 tokens to avoid GPT-3 API errors feat(gpt-shell.py): add support for truncating output prompt to 1024 tokens to avoid GPT-3 API errors feat(gpt-shell.py): add support for analyzing command output and prompting user to run the command feat(gpt-shell.py): add support for handling errors when running commands and prompting user to analyze the error output

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 The `gpt-shell.py` script is a Python command-line interface that uses OpenAI's GPT-3 API to provide suggested commands for Linux shell commands. The script prompts the user to input a command and then provides a suggested command to execute based on the input. The script uses the `revChatGPT.V3` module to interface with the GPT-3 API and requires an API key to be set as an environment variable. The script is designed to be run in a terminal and provides a system prompt for the user to input commands. If the suggested command requires multiple steps, the script will attempt to combine them together. The output of the command is analyzed by the GPT-3 model to provide a description of the suggested command.
 
+![Screen 1](screen1.png "Screen 1")
 ## Installation
 
 1. Clone the repository to your local machine.
--- gpt-shell.py
+++ gpt-shell.py
@@ -3,71 +3,95 @@ import subprocess
 from revChatGPT.V3 import Chatbot
 from termcolor import colored
 
-system_prompt = """Provide only bash commands for Linux without any description.
+
+def get_api_key():
+    api_key = os.getenv("OPENAI_API_KEY", "")
+    if api_key == "":
+        print(colored("Error: OPENAI_API_KEY is not set", "red"))
+        exit(1)
+    return api_key
+
+
+def truncate_prompt(prompt, max_tokens):
+    num_tokens = len(prompt.split())
+    if num_tokens > max_tokens:
+        print(colored("Input prompt is too long, truncating...", "yellow"))
+        prompt = " ".join(prompt.split()[:max_tokens])
+    return prompt
+
+
+def run_command(command):
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, errors = process.communicate()
+    exit_code = process.wait()
+
+    print(
+        colored(f"=== Command output - exit({exit_code})\n{output.decode()}", "magenta"))
+    if errors.decode() != "":
+        print(
+            colored(f"{errors.decode()}", "red"))
+
+    prompt = f"Analyze command output\n{output.decode()}"
+    if errors.decode() != "":
+        prompt += f"\nstderr:\n{errors.decode()}"
+
+    return prompt
+
+
+def analyze_command(chatbot, command):
+    response = chatbot.ask(command)
+    print(colored(f"=== Response\n{response}", "yellow"))
+
+    while "CMD: " in response:
+        commands = [line.replace(
+            "CMD: ", "") for line in response.splitlines() if line.startswith("CMD: ")]
+        command = "; ".join(commands)
+        if command == "":
+            break
+
+        print(colored(f"=== Command\n{command}", "blue"))
+        run = input(
+            colored(f"Do you want to run the command? (y/N): ", "green"))
+
+        if run.lower() == "y":
+            prompt = run_command(command)
+            # prompt = truncate_prompt(prompt, max_tokens=1024)
+
+            response = chatbot.ask(prompt)
+            print(colored(f"=== Response\n{response}", "yellow"))
+        else:
+            break
+
+
+def main():
+    system_prompt = """Provide only bash commands for Linux without any description.
 If there is a lack of details, provide most logical solution.
 Ensure the output is a valid shell command.
 If multiple steps required try to combine them together.
 Before command add "CMD: ".
 """
 
-api_key = os.getenv("OPENAI_API_KEY", "")
-if api_key == "":
-    print(colored("Error: OPENAI_API_KEY is not set", "red"))
-    exit(1)
+    api_key = get_api_key()
+    chatbot = Chatbot(api_key=api_key, system_prompt=system_prompt, max_tokens=1024, truncate_limit=1024)
 
-chatbot = Chatbot(api_key=api_key, system_prompt=system_prompt)
+    while True:
+        try:
+            prompt = input(colored("ChatGPT: ", "green"))
+            # prompt = truncate_prompt(prompt, max_tokens=2048)
+            analyze_command(chatbot, prompt)
+        except subprocess.CalledProcessError as e:
+            print(colored(
+                f"Error: Command failed with exit code {e.returncode}: {e.output}", "red"))
+        except KeyboardInterrupt:
+            print(colored("Exiting...", "yellow"))
+            exit(0)
+        except EOFError:
+            print(colored("Exiting...", "yellow"))
+            exit(0)
+        except Exception as e:
+            print(colored(f"Error of type {type(e).__name__}: {e}", "red"))
 
-while True:
-    try:
-        prompt = input(colored("ChatGPT: ", "green"))
-        response = chatbot.ask(prompt)
-        print(colored(f"=== Response\n{response}", "yellow"))
 
-        while "CMD: " in response:
-            commands = [line.replace(
-                "CMD: ", "") for line in response.splitlines() if line.startswith("CMD: ")]
-            command = "; ".join(commands)
-            if command == "":
-                break
-
-            print(colored(f"=== Command\n{command}", "blue"))
-            run_command = input(
-                colored(f"Do you want to run the command? (y/N): ", "green"))
-
-            if run_command.lower() == "y":
-                process = subprocess.Popen(
-                    command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                output, errors = process.communicate()
-                exit_code = process.wait()
-
-                if exit_code != 0:
-                    print(colored(
-                        f"=== Error: Command failed - exit({exit_code}):\n{errors.decode()}"
-                        + (f"{output.decode()}" if output else ""), "red"))
-                    prompt = f"Analyze command error:\n{errors.decode()}\n{output.decode()}"
-                else:
-                    print(
-                        colored(f"=== Command output\n{output.decode()}", "magenta"))
-                    prompt = f"Analyze command output:\n{output.decode()}"
-
-                response = chatbot.ask(prompt)
-                if exit_code != 0:
-                    print(
-                        colored(f"=== Response for error\n{response}", "yellow"))
-                else:
-                    print(
-                        colored(f"=== Response for output\n{response}", "yellow"))
-            else:
-                break
-
-    except subprocess.CalledProcessError as e:
-        print(colored(
-            f"Error: Command failed with exit code {e.returncode}: {e.output}", "red"))
-    except KeyboardInterrupt:
-        print(colored("Exiting...", "yellow"))
-        exit(0)
-    except EOFError:
-        print(colored("Exiting...", "yellow"))
-        exit(0)
-    except Exception as e:
-        print(colored(f"Error of type {type(e).__name__}: {e}", "red"))
+if __name__ == "__main__":
+    main()
