0e4c732543621c40963660d2e6d856b6aa974d1b
bobergda <bobergda@gmail.com>
2023-05-23 09:11:18 +0200

    refactor(gpt-shell.py): rename functions to improve semantics feat(gpt-shell.py): change function names to better reflect their purpose and improve code readability docs(gpt-shell.py): update comments to improve code readability and provide more context

--- gpt-shell.py
+++ gpt-shell.py
@@ -12,75 +12,75 @@ def get_api_key():
     return api_key
 
 
-def truncate_prompt(prompt, max_tokens):
+def trim_prompt(prompt, max_length):
     num_tokens = len(prompt.split())
-    if num_tokens > max_tokens:
+    if num_tokens > max_length:
         print(colored("Input prompt is too long, truncating...", "yellow"))
-        prompt = " ".join(prompt.split()[:max_tokens])
+        prompt = " ".join(prompt.split()[:max_length])
     return prompt
 
 
-def run_command(command):
-    process = subprocess.Popen(
+def execute_command(command):
+    command_process = subprocess.Popen(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, errors = process.communicate()
-    exit_code = process.wait()
+    command_output, command_errors = command_process.communicate()
+    exit_status = command_process.wait()
 
     print(
-        colored(f"=== Command output - exit({exit_code})\n{output.decode()}", "magenta"))
-    if errors.decode() != "":
+        colored(f"=== Command output - exit({exit_status})\n{command_output.decode()}", "magenta"))
+    if command_errors.decode() != "":
         print(
-            colored(f"{errors.decode()}", "red"))
+            colored(f"{command_errors.decode()}", "red"))
 
-    prompt = f"Analyze command output\n{output.decode()}"
-    if errors.decode() != "":
-        prompt += f"\nstderr:\n{errors.decode()}"
+    prompt = f"Analyze command output\n{command_output.decode()}"
+    if command_errors.decode() != "":
+        prompt += f"\nstderr:\n{command_errors.decode()}"
 
     return prompt
 
 
-def analyze_command(chatbot, input_prompt):
-    chatbot_response = chatbot.ask(input_prompt)
-    print(colored(f"=== Response\n{chatbot_response}", "yellow"))
+def interpret_command(chatbot, user_prompt):
+    chatbot_reply = chatbot.ask(user_prompt)
+    print(colored(f"=== Response\n{chatbot_reply}", "yellow"))
 
-    while "```" in chatbot_response:
-        split_response = chatbot_response.split('```')
-        extracted_commands = split_response[1::2]
-        commands_string = '\n'.join(command.strip()
-                                    for command in extracted_commands if command.strip() != "")
-        if commands_string == "":
+    while "```" in chatbot_reply:
+        split_reply = chatbot_reply.split('```')
+        parsed_commands = split_reply[1::2]
+        commands_summary = '\n'.join(command.strip()
+                                     for command in parsed_commands if command.strip() != "")
+        if commands_summary == "":
             break
 
-        print(colored(f"=== Command\n{commands_string}", "blue"))
-        user_input = input(
+        print(colored(f"=== Command\n{commands_summary}", "blue"))
+        run_confirmation = input(
             colored(f"Do you want to run the command? (y/N): ", "green"))
 
-        if user_input.lower() == "y":
-            input_prompt = run_command(commands_string)
+        if run_confirmation.lower() == "y":
+            user_prompt = execute_command(commands_summary)
 
-            chatbot_response = chatbot.ask(input_prompt)
-            print(colored(f"=== Response\n{chatbot_response}", "yellow"))
+            chatbot_reply = chatbot.ask(user_prompt)
+            print(colored(f"=== Response\n{chatbot_reply}", "yellow"))
         else:
             break
 
 
 def main():
-    system_prompt = """Provide bash commands for Linux.
-If there is a lack of details, provide most logical solution.
+    chatbot_prompt = """Provide bash commands for Linux.
+If there is a lack of details, provide the most logical solution.
 Ensure the output is a valid shell command.
 If multiple steps required try to combine them together.
 """
 # Before command add "CMD: ".
 
     api_key = get_api_key()
-    chatbot = Chatbot(api_key=api_key, system_prompt=system_prompt,
-                      max_tokens=1024, truncate_limit=1024)
+    chatbot_instance = Chatbot(api_key=api_key, system_prompt=chatbot_prompt,
+                               max_tokens=1024, truncate_limit=1024)
 
     while True:
         try:
-            prompt = input(colored("ChatGPT: ", "green"))
-            # prompt = truncate_prompt(prompt, max_tokens=2048)
-            analyze_command(chatbot, prompt)
+            user_input = input(colored("ChatGPT: ", "green"))
+            # user_input = trim_prompt(user_input, max_tokens=2048)
+            interpret_command(chatbot_instance, user_input)
         except subprocess.CalledProcessError as e:
             print(colored(
                 f"Error: Command failed with exit code {e.returncode}: {e.output}", "red"))
