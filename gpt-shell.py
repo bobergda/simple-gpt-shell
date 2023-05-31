f3f53fc4b736792b8124a1934186ecbc213bea56
bobergda <bobergda@gmail.com>
2023-05-31 17:45:43 +0200

    refactor(gpt-shell.py): replace revChatGPT library with OpenAI API for chatbot functionality feat(gpt-shell.py): add function to format user input into chat-style prompt and send it to OpenAI API for chatbot response feat(gpt-shell.py): add option for manual command mode and execute user input as shell command refactor(gpt-shell.py): remove unused dependencies and add keyboard to requirements.txt

--- gpt-shell.py
+++ gpt-shell.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-from revChatGPT.V3 import Chatbot
+import openai
 from termcolor import colored
 import platform
 import distro
@@ -12,7 +12,22 @@ def get_api_key():
     if api_key == "":
         print(colored("Error: OPENAI_API_KEY is not set", "red"))
         exit(1)
-    return api_key
+    openai.api_key = api_key
+
+
+def ask_chatbot(prompt, system_prompt):
+    # format the prompt into a chat-style prompt
+    chat_prompt = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt}
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # or whichever model you are using
+        messages=chat_prompt,
+    )
+    # return the assistant's reply
+    response_string = response['choices'][0]['message']['content']
+    return response_string
 
 
 def execute_command(command):
@@ -21,15 +36,30 @@ def execute_command(command):
     return result
 
 
-def interpret_command(chatbot, user_prompt):
-    chatbot_reply = chatbot.ask(user_prompt)
+def interpret_command(system_prompt, user_prompt):
+    if user_prompt == "e":
+        print(colored("Manual command mode activated. Please enter your command:", "green"))
+        command_str = input()
+        command_output = execute_command(command_str)
+        print(colored(f"=== Command output\n{command_output.stdout}", "magenta"))
+        if command_output.stderr != "":
+            print(colored(f"=== Command error\n{command_output.stderr}", "red"))
+        # construct a chat message from the command output and pass it to the chat
+        command_chat_msg = f"I executed the '{command_str}' myself and got this output:\n" + command_output.stdout
+        if command_output.stderr != "":
+            command_chat_msg += "\nError output:\n" + command_output.stderr
+
+        chatbot_reply = ask_chatbot(command_chat_msg, system_prompt)
+    else:
+        chatbot_reply = ask_chatbot(user_prompt, system_prompt)
+
     print(colored(f"=== Response\n{chatbot_reply}", "yellow"))
 
     while "```" in chatbot_reply:
         split_reply = chatbot_reply.split('```')
         commands_list = split_reply[1::2]
         commands_str = '\n'.join(command.strip()
-                                 for command in commands_list if command.strip() != "")
+                                for command in commands_list if command.strip() != "")
         if commands_str == "":
             break
 
@@ -66,7 +96,7 @@ def interpret_command(chatbot, user_prompt):
                     colored(f"=== Command error\n{command_output.stderr}", "red"))
                 prompt += f"\n{command_output.stderr}"
 
-            chatbot_reply = chatbot.ask(prompt)
+            chatbot_reply = ask_chatbot(user_prompt, system_prompt)
             print(colored(f"=== Response\n{chatbot_reply}", "yellow"))
         else:
             break
@@ -87,22 +117,20 @@ def get_os_and_shell_names():
 
 def main():
     os_name, shell_name = get_os_and_shell_names()
-
-    chatbot_prompt = f"""Provide {shell_name} commands for {os_name}.
+    system_prompt = f"""Provide {shell_name} commands for {os_name}.
 If details are missing, suggest the most logical solution.
 Ensure valid shell command output.
 For multiple steps, combine them if possible.
 Use ``` only to separate commands.
 """
-    print(colored(f"=== ChatGPT system_prompt\n{chatbot_prompt}", "yellow"))
-    api_key = get_api_key()
-    chatbot_instance = Chatbot(
-        api_key=api_key, system_prompt=chatbot_prompt, truncate_limit=1024)
+
+    print(colored(f"=== ChatGPT system_prompt\n{system_prompt}", "yellow"))
+    get_api_key()
 
     while True:
         try:
             user_input = input(colored("ChatGPT: ", "green"))
-            interpret_command(chatbot_instance, user_input)
+            interpret_command(system_prompt, user_input)
         except subprocess.CalledProcessError as e:
             print(colored(
                 f"Error: Command failed with exit code {e.returncode}: {e.output}", "red"))
