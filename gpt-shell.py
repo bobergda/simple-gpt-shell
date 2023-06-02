d82dbffc670d5f9f84444ae5ff2499ebd5cc5747
bobergda <bobergda@gmail.com>
2023-06-02 16:00:18 +0200

    refactor(gpt-shell.py): improve code readability and maintainability by splitting long lines into multiple lines docs(gpt-shell.py): add comments to clarify code functionality and usage
    refactor(gpt-shell.py): restructure code into classes and methods for better organization and readability
    feat(gpt-shell.py): add OpenAIHelper class to handle OpenAI API requests
    feat(gpt-shell.py): add CommandHelper class to handle shell command execution and output printing
    feat(gpt-shell.py): add OSHelper class to get information about the operating system and shell
    feat(gpt-shell.py): add Application class to handle user input and execute commands using OpenAI and CommandHelper classes

--- gpt-shell.py
+++ gpt-shell.py
@@ -8,172 +8,181 @@ import distro
 import tiktoken
 
 
-# define system_prompt as a global variable
-global system_prompt
-MAX_PROMPT_TOKENS = 4096
-encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+class OpenAIHelper:
+    def __init__(self, system_prompt, max_prompt_tokens=4096):
+        self.api_key = os.getenv("OPENAI_API_KEY", "")
+        self.system_prompt = system_prompt
+        self.max_prompt_tokens = max_prompt_tokens
+        self.encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        if self.api_key == "":
+            print(colored("Error: OPENAI_API_KEY is not set", "red"))
+            exit(1)
+        openai.api_key = self.api_key
 
-
-def load_openai_api_key():
-    api_key = os.getenv("OPENAI_API_KEY", "")
-    if api_key == "":
-        print(colored("Error: OPENAI_API_KEY is not set", "red"))
-        exit(1)
-    openai.api_key = api_key
-
-
-def request_chatbot_response(prompt):
-    system_prompt_tokens = encoding.encode(system_prompt)
-    max_tokens = MAX_PROMPT_TOKENS - 1024 - len(system_prompt_tokens)
-    tokens = encoding.encode(prompt)
-    if len(tokens) > max_tokens:
-        prompt = encoding.decode(tokens[:max_tokens])
-        # remove last line to avoid incomplete output
-        prompt = prompt[:prompt.rfind("\n")]
-        print(colored(
-            f"Warning: prompt was truncated to {max_tokens} tokens:\n{prompt}", "yellow"))
-
-    chat_prompt = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": prompt}
-    ]
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # or whichever model you are using
-        messages=chat_prompt,
-    )
-    response_string = response['choices'][0]['message']['content']
-    return response_string
-
-
-def run_shell_command(command):
-    result = subprocess.run(
-        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    if result.stdout != "":
-        stdout_lines = result.stdout.split("\n")
-        for i, line in enumerate(stdout_lines):
-            if "KEY" in line:
-                post_key_content = line[line.find("KEY") + 3:].strip()
-                if "=" in post_key_content:
-                    stdout_lines[i] = line.split("=")[0] + "=<API_KEY>"
-        result.stdout = "\n".join(stdout_lines)
-    return result
-
-
-def interpret_and_execute_command(user_prompt):
-    if user_prompt == "e":
-        manual_command_mode()
-    else:
-        auto_command_mode(user_prompt)
-
-
-def manual_command_mode():
-    print(colored("Manual command mode activated. Please enter your command:", "green"))
-    command_str = input()
-    command_output = run_shell_command(command_str)
-    prompt = f"Analyze command '{command_str}' output:\n" + \
-        command_output.stdout
-    if command_output.stderr != "":
-        prompt += "\nError output:\n" + command_output.stderr
-    print_command_output(command_output)
-
-    chatbot_reply = request_chatbot_response(prompt)
-    print_chatbot_response(chatbot_reply)
-    execute_commands_in_chatbot_response(chatbot_reply)
-
-
-def auto_command_mode(user_prompt):
-    chatbot_reply = request_chatbot_response(user_prompt)
-    print_chatbot_response(chatbot_reply)
-    execute_commands_in_chatbot_response(chatbot_reply)
-
-
-def execute_commands_in_chatbot_response(chatbot_reply):
-    while "```" in chatbot_reply:
-        split_reply = chatbot_reply.split('```')
-        commands_list = split_reply[1::2]
-        command_str = '\n'.join(command.strip()
-                                for command in commands_list if command.strip() != "")
-        if command_str == "":
-            break
-
-        print(colored(f"=== Command\n{command_str}", "blue"))
-
-        action = input(
-            colored(f"Do you want to run (y) or edit (e) the command? (y/e/N): ", "green"))
-
-        if action.lower() == "e":
-            command_str = input(
-                colored("Enter the modified command: ", "cyan"))
-            action = input(
-                colored(f"Do you want to run the command? (y/N): ", "green"))
-
-        if action.lower() == "y":
-            command_output = run_shell_command(command_str)
-            prompt = f"Analyze command output:\n{command_output.stdout}"
-            if command_output.stderr != "":
-                prompt += "\nError output:\n" + command_output.stderr
-            print_command_output(command_output)
-
-            chatbot_reply = request_chatbot_response(prompt)
-            print_chatbot_response(chatbot_reply)
-        else:
-            break
-
-
-def print_command_output(command_output):
-    print(colored(f"=== Command output\n{command_output.stdout}", "magenta"))
-    if command_output.stderr != "":
-        print(colored(f"=== Command error\n{command_output.stderr}", "red"))
-
-
-def print_chatbot_response(chatbot_reply):
-    print(colored(f"=== Response\n{chatbot_reply}", "yellow"))
-
-
-def get_os_and_shell_info():
-    os_name = platform.system()
-    shell_name = os.path.basename(os.environ.get("SHELL", "bash"))
-    if os_name == "Linux":
-        os_name += " " + distro.name()
-    elif os_name == "Darwin":
-        os_name += " " + platform.mac_ver()[0]
-    elif os_name == "Windows":
-        os_name += " " + platform.release()
-    return os_name, shell_name
-
-
-def main():
-    os_name, shell_name = get_os_and_shell_info()
-    # use the global system_prompt variable
-    global system_prompt
-    system_prompt = f"""Provide {shell_name} commands for {os_name}.
-If details are missing, suggest the most logical solution.
-Ensure valid shell command output.
-For multiple steps, combine them if possible.
-Use ``` only to separate commands.
-"""
-    print(
-        colored(f"Your current environment: Shell={shell_name}, OS={os_name}", "green"))
-    print(colored("Type 'e' to enter manual command mode\n", "green"))
-    load_openai_api_key()
-
-    while True:
-        try:
-            user_input = input(colored("ChatGPT: ", "green"))
-            interpret_and_execute_command(user_input)
-        except subprocess.CalledProcessError as e:
+    def request_chatbot_response(self, prompt):
+        system_prompt_tokens = self.encoding.encode(self.system_prompt)
+        max_tokens = self.max_prompt_tokens - 1024 - len(system_prompt_tokens)
+        tokens = self.encoding.encode(prompt)
+        if len(tokens) > max_tokens:
+            prompt = self.encoding.decode(tokens[:max_tokens])
+            # remove last line to avoid incomplete output
+            prompt = prompt[:prompt.rfind("\n")]
             print(colored(
-                f"Error: Command failed with exit code {e.returncode}: {e.output}", "red"))
-        except KeyboardInterrupt:
-            print(colored("Exiting...", "yellow"))
-            exit(0)
-        except EOFError:
-            print(colored("Exiting...", "yellow"))
-            exit(0)
-        except Exception as e:
-            print(colored(f"Error of type {type(e).__name__}: {e}", "red"))
+                f"Warning: prompt was truncated to {max_tokens} tokens:\n{prompt}", "yellow"))
+
+        chat_prompt = [
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": prompt}
+        ]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # or whichever model you are using
+            messages=chat_prompt,
+        )
+        response_string = response['choices'][0]['message']['content']
+        return response_string
+
+
+class CommandHelper:
+    @staticmethod
+    def run_shell_command(command):
+        result = subprocess.run(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        if result.stdout != "":
+            stdout_lines = result.stdout.split("\n")
+            for i, line in enumerate(stdout_lines):
+                if "KEY" in line:
+                    post_key_content = line[line.find("KEY") + 3:].strip()
+                    if "=" in post_key_content:
+                        stdout_lines[i] = line.split("=")[0] + "=<API_KEY>"
+            result.stdout = "\n".join(stdout_lines)
+        return result
+
+    @staticmethod
+    def print_command_output(command_output):
+        print(
+            colored(f"=== Command output\n{command_output.stdout}", "magenta"))
+        if command_output.stderr != "":
+            print(
+                colored(f"=== Command error\n{command_output.stderr}", "red"))
+
+
+class OSHelper:
+    @staticmethod
+    def get_os_and_shell_info():
+        os_name = platform.system()
+        shell_name = os.path.basename(os.environ.get("SHELL", "bash"))
+        if os_name == "Linux":
+            os_name += " " + distro.name()
+        elif os_name == "Darwin":
+            os_name += " " + platform.mac_ver()[0]
+        elif os_name == "Windows":
+            os_name += " " + platform.release()
+        return os_name, shell_name
+
+
+class Application:
+    def __init__(self, openai_helper, command_helper, os_helper):
+        self.openai_helper = openai_helper
+        self.command_helper = command_helper
+        self.os_helper = os_helper
+
+    def interpret_and_execute_command(self, user_prompt):
+        if user_prompt == "e":
+            self.manual_command_mode()
+        else:
+            self.auto_command_mode(user_prompt)
+
+    def manual_command_mode(self):
+        print(colored("Manual command mode activated. Please enter your command:", "green"))
+        command_str = input()
+        command_output = self.command_helper.run_shell_command(command_str)
+        prompt = f"Analyze command '{command_str}' output:\n" + \
+            command_output.stdout
+        if command_output.stderr != "":
+            prompt += "\nError output:\n" + command_output.stderr
+        self.command_helper.print_command_output(command_output)
+
+        chatbot_reply = self.openai_helper.request_chatbot_response(prompt)
+        self.print_chatbot_response(chatbot_reply)
+        self.execute_commands_in_chatbot_response(chatbot_reply)
+
+    def auto_command_mode(self, user_prompt):
+        chatbot_reply = self.openai_helper.request_chatbot_response(
+            user_prompt)
+        self.print_chatbot_response(chatbot_reply)
+        self.execute_commands_in_chatbot_response(chatbot_reply)
+
+    def execute_commands_in_chatbot_response(self, chatbot_reply):
+        while "```" in chatbot_reply:
+            split_reply = chatbot_reply.split('```')
+            commands_list = split_reply[1::2]
+            command_str = '\n'.join(command.strip()
+                                    for command in commands_list if command.strip() != "")
+            if command_str == "":
+                break
+
+            print(colored(f"=== Command\n{command_str}", "blue"))
+
+            action = input(
+                colored(f"Do you want to run (y) or edit (e) the command? (y/e/N): ", "green"))
+
+            if action.lower() == "e":
+                command_str = input(
+                    colored("Enter the modified command: ", "cyan"))
+                action = input(
+                    colored(f"Do you want to run the command? (y/N): ", "green"))
+
+            if action.lower() == "y":
+                command_output = self.command_helper.run_shell_command(
+                    command_str)
+                prompt = f"Analyze command output:\n{command_output.stdout}"
+                if command_output.stderr != "":
+                    prompt += "\nError output:\n{command_output.stderr}"
+                self.command_helper.print_command_output(command_output)
+
+                chatbot_reply = self.openai_helper.request_chatbot_response(
+                    prompt)
+                self.print_chatbot_response(chatbot_reply)
+            else:
+                break
+
+    @staticmethod
+    def print_chatbot_response(chatbot_reply):
+        print(colored(f"=== Response\n{chatbot_reply}", "yellow"))
+
+    def run(self):
+        os_name, shell_name = self.os_helper.get_os_and_shell_info()
+        print(
+            colored(f"Your current environment: Shell={shell_name}, OS={os_name}", "green"))
+        print(colored("Type 'e' to enter manual command mode\n", "green"))
+
+        while True:
+            try:
+                user_input = input(colored("ChatGPT: ", "green"))
+                self.interpret_and_execute_command(user_input)
+            except subprocess.CalledProcessError as e:
+                print(colored(
+                    f"Error: Command failed with exit code {e.returncode}: {e.output}", "red"))
+            except KeyboardInterrupt:
+                print(colored("Exiting...", "yellow"))
+                exit(0)
+            except EOFError:
+                print(colored("Exiting...", "yellow"))
+                exit(0)
+            except Exception as e:
+                print(colored(f"Error of type {type(e).__name__}: {e}", "red"))
 
 
 if __name__ == "__main__":
-    main()
+    system_prompt = f"""Provide shell commands for the operating system.
+    If details are missing, suggest the most logical solution.
+    Ensure valid shell command output.
+    For multiple steps, combine them if possible.
+    Use ``` only to separate commands.
+    """
+    openai_helper = OpenAIHelper(system_prompt)
+    command_helper = CommandHelper()
+    os_helper = OSHelper()
+    application = Application(openai_helper, command_helper, os_helper)
+    application.run()
